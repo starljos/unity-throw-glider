0668e6e787f3efbfd023ada92288d6a3199f9991
Marek Polasik <marx432@gmail.com>
2020-05-31 01:55:41 +0200

    working draft

---
+++ Assets/Scripts/Glider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glider : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public Transform earthTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // gameObject.Find("glider-main").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            Debug.Log("Collision with Earth");
+            this.landGlider();
+        }
+
+        if (col.gameObject.tag == "Glider")
+        {
+            Debug.Log("Collision with Glider");
+            this.crashGlider();
+        }
+    }
+
+    public void releaseGlider()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    void landGlider()
+    {
+        GetComponent<Animator>().enabled = false;
+        rb.bodyType = RigidbodyType2D.Static;
+        gameObject.transform.SetParent(earthTransform);
+    }
+
+    void crashGlider()
+    {
+        gameObject.transform.Find("glider-main").gameObject.active = false;
+        gameObject.transform.Find("glider-frak").gameObject.active = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Glider CurrentGlider;
+    public GameObject gliderPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.spawnGlider();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            CurrentGlider.releaseGlider();
+            StartCoroutine(SpawnGliderAfterDelay(1));
+        }
+    }
+
+    IEnumerator SpawnGliderAfterDelay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        this.spawnGlider();
+    }
+
+    void spawnGlider()
+    {
+        GameObject newGlider;
+        newGlider = Instantiate(gliderPrefab);
+        newGlider.transform.SetParent(gameObject.transform);
+        CurrentGlider = newGlider.GetComponent<Glider>();
+        newGlider.active = true;
+        Debug.Log("SPAWNED");
+
+    }
+}
